f07ad4da3d05cb2d942abc0de8213701e63d3225
azadeh pazhohiyani <azadehpazhuhani@gmail.com>
2023-10-15 23:01:17 +0330

    add web2-tailwind & edit web2-6

---
+++ web2-tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+     "bgHeader":"rgba(22, 22, 23, .8)",
+     "bgDropdDown" : "rgba(22, 22, 23, 1)"
+     })
+  },
+  plugins: [],
+}
+
